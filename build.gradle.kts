d3a78b51bc0226c7e96bf6060af3c3f52b22e773
Natan <xpenatan@gmail.com>
2026-07-22 13:04:52 -0300

    Update easy publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     id("java")
+    id("com.github.xpenatan.easy-publishing") version "0.1.0"
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
 }
+
+LibExt.isRelease = rootProject.extra["easyPublishing.releaseRequested"] as Boolean
 
 buildscript {
     repositories {
@@ -53,6 +56,63 @@ allprojects  {
     }
 }
 
+val publishingModules = listOf(
+    ":backends:gdx:gdx-shared-impl",
+    ":backends:gdx:gdx-gl-impl",
+    ":backends:gdx:gdx-wgpu-impl",
+    ":backends:fdx:fdx-impl",
+    ":imgui:core",
+    ":imgui:shared:jni",
+    ":imgui:shared:c",
+    ":imgui:desktop:jni",
+    ":imgui:desktop:ffm",
+    ":imgui:desktop:c",
+    ":imgui:web:wasm",
+    ":imgui:android:jni",
+    ":imgui:android:c",
+    ":extensions:imlayout:imlayout-core",
+    ":extensions:imlayout:imlayout-jni",
+    ":extensions:imlayout:imlayout-ffm",
+    ":extensions:imlayout:imlayout-web",
+    ":extensions:imlayout:imlayout-c",
+    ":extensions:ImGuiColorTextEdit:textedit-core",
+    ":extensions:ImGuiColorTextEdit:textedit-jni",
+    ":extensions:ImGuiColorTextEdit:textedit-ffm",
+    ":extensions:ImGuiColorTextEdit:textedit-web",
+    ":extensions:ImGuiColorTextEdit:textedit-c",
+    ":extensions:imgui-node-editor:nodeeditor-core",
+    ":extensions:imgui-node-editor:nodeeditor-jni",
+    ":extensions:imgui-node-editor:nodeeditor-ffm",
+    ":extensions:imgui-node-editor:nodeeditor-web",
+    ":extensions:imgui-node-editor:nodeeditor-c",
+)
+
+easyPublishing {
+    modules(publishingModules)
+
+    groupId.set(LibExt.groupId)
+    releaseVersion.set(providers.gradleProperty("version"))
+    snapshotVersion.set("-SNAPSHOT")
+
+    snapshotRepositoryUrl.set("https://central.sonatype.com/repository/maven-snapshots/")
+    releaseRepositoryUrl.set("https://central.sonatype.com")
+    username.set(providers.environmentVariable("CENTRAL_PORTAL_USERNAME"))
+    password.set(providers.environmentVariable("CENTRAL_PORTAL_PASSWORD"))
+    signingKey.set(providers.environmentVariable("SIGNING_KEY"))
+    signingPassword.set(providers.environmentVariable("SIGNING_PASSWORD"))
+
+    pomName.set(LibExt.libName)
+    pomDescription.set("ImGui Java Bindings")
+    projectUrl.set("https://github.com/xpenatan/jImGui")
+
+    developerId.set("Xpe")
+    developerName.set("Natan")
+
+    scmUrl.set("https://github.com/xpenatan/jImGui")
+    scmConnection.set("scm:git:https://github.com/xpenatan/jImGui.git")
+    scmDeveloperConnection.set("scm:git:ssh://git@github.com/xpenatan/jImGui.git")
+}
+
 tasks.register("download_all_sources") {
     group = "imgui"
     description = "Download all sources"
@@ -67,5 +127,3 @@ tasks.register("download_all_sources") {
     tasks.findByPath(source2)!!.mustRunAfter(source1)
     tasks.findByPath(source3)!!.mustRunAfter(source2)
 }
-
-apply(plugin = "publish")
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,12 +3,5 @@ plugins {
 }
 
 repositories {
-    maven {
-        url = uri("https://central.sonatype.com/repository/maven-snapshots/")
-    }
     mavenCentral()
-}
-
-dependencies {
-    implementation("com.github.xpenatan:easy-publishing:-SNAPSHOT")
 }
